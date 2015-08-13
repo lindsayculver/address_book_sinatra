0c39b3bb045e8ad4fb9533c7a963c577759e60e1
tony navarro & lindsay culver <student@epicodus.com>
2015-08-13 11:34:35 -0700

    adds fills out contact form to handle all of the other class files and passes all the specs

--- lib/contact.rb
+++ lib/contact.rb
@@ -1,49 +1,32 @@
-require('pry')
-
 class Contact
   @@contacts = []
-  define_method(:initialize) do |name, email, address, phone|
-    @name = name
-    @email = email
-    @address = address
-    @phone = phone
-    @id = @@contacts.length.+(1)
+  define_method(:initialize) do |email_handle|
+    @email_handle = email_handle
+    @id = @@contacts.length().+(1)
+    @details = []
   end
-
-  define_singleton_method(:all) do
-    @@contacts
+  define_method(:email_handle) do
+    @email_handle
   end
-
-  define_method(:save) do
-    @@contacts.push(self)
-  end
-
-  define_singleton_method(:clear) do
-    @@contacts = []
-  end
-
-  define_method(:name) do
-    @name
-  end
-
-  define_method(:email) do
-    @email
-  end
-
-  define_method(:address) do
-    @address
-  end
-  define_method(:phone) do
-    @phone
-  end
-
   define_method(:id) do
     @id
   end
-  define_singleton_method(:find) do |ident|
+  define_method(:details) do
+    @details
+  end
+  define_singleton_method(:all) do
+    @@contacts
+  end
+  define_method(:save) do
+    @@contacts.push(self)
+  end
+  define_singleton_method(:clear) do
+    @@contacts = []
+  end
+  define_singleton_method(:find) do |id|
     found_contact = nil
     @@contacts.each() do |contact|
-      if contact.id().eql?(ident)
+      if contact.id.eql?(id)
         found_contact = contact
       end
     end
--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -1,71 +1,46 @@
-# require('rspec')
-# require('address_book')
-#
-# describe('Contact') do
-#   describe('.all') do
-#     it('shows the empty array of contacts') do
-#       expect(Contact.all()).to(eq([]))
-#     end
-#   end
-#
-#   describe('#save') do
-#     it('will save the contact into the @@contacts array') do
-#       test_contact = Contact.new('Jimmy', 'jimmy@email.com', '123 fake. st', '3605555555')
-#       expect(test_contact.save()).to(eq([test_contact]))
-#     end
-#   end
-#   describe('.clear') do
-#     it('it clears the @@contacts array of all contacts') do
-#       test_contact = Contact.new('Bill', 'bill@email.com', '123 fake. st', '3605555555')
-#       test_contact.save()
-#       expect(Contact.clear()).to(eq([]))
-#     end
-#   end
-#
-#   describe('#name') do
-#     it('returns a name when the method is called') do
-#       test_contact = Contact.new('Sally', 'sally@email.com', '123 fake. st', '3605555555')
-#       test_contact.save()
-#       expect(test_contact.name()).to(eq('Sally'))
-#     end
-#   end
-#
-#   describe('#email') do
-#     it('returns the email when the method is called') do
-#       test_contact = Contact.new('Tim', 'tim@email.com', '123 fake st.', '3605555555')
-#       test_contact.save()
-#       expect(test_contact.email()).to(eq('tim@email.com'))
-#     end
-#   end
-#
-#   describe('#address') do
-#     it('returns the address when the method is called') do
-#       test_contact = Contact.new('Pam', 'pam@email.com', '123 fake st.', '3605555555')
-#       test_contact.save()
-#       expect(test_contact.address()).to(eq('123 fake st.'))
-#     end
-#   end
-#   describe('#phone') do
-#     it('returns the phone number when the method is called') do
-#       test_contact = Contact.new('Gunther', 'gunther@email.com', '123 fake st.','3605555555')
-#       test_contact.save()
-#       expect(test_contact.phone()).to(eq('3605555555'))
-#     end
-#   end
-#
-#   describe('#id') do
-#     it('returns unique id of contact stored in array') do
-#       Contact.clear()
-#       test_contact = Contact.new('Monica', 'monica@email.com', '123 fake st.','3605555555')
-#       test_contact.save()
-#       expect(test_contact.id()).to(eq(1))
-#     end
-#   end
-#   describe('.find') do
-#     it('finds the desired contact by id and returns it') do
-#       test_contact = Contact.new('Joey', 'joey@email.com', '123 fake st.','3605555555')
-#       test_contact.save()
-#       expect(Contact.find(test_contact.id())).to(eq(test_contact))
-#     end
-#   end
-# end
+require('rspec')
+require('contact')
+
+describe('Contact') do
+  describe('.all') do
+    it('shows the empty array of contacts') do
+      expect(Contact.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('will save the contact into the @@contacts array') do
+      test_contact = Contact.new('jimmy@email.com')
+      expect(test_contact.save()).to(eq([test_contact]))
+    end
+  end
+  describe('.clear') do
+    it('it clears the @@contacts array of all contacts') do
+      test_contact = Contact.new('bill@email.com')
+      test_contact.save()
+      expect(Contact.clear()).to(eq([]))
+    end
+  end
+  describe('#email_handle') do
+    it('returns the email_handle when the method is called') do
+      test_contact = Contact.new('tim@email.com')
+      test_contact.save()
+      expect(test_contact.email_handle()).to(eq('tim@email.com'))
+    end
+  end
+  describe('#id') do
+    it('returns unique id of contact stored in array') do
+      Contact.clear()
+      test_contact = Contact.new('monica@email.com')
+      test_contact.save()
+      expect(test_contact.id()).to(eq(1))
+    end
+  end
+  describe('.find') do
+    it('finds the desired contact by id and returns it') do
+      test_contact = Contact.new('joey@email.com')
+      test_contact.save()
+      expect(Contact.find(test_contact.id())).to(eq(test_contact))
+    end
+  end
+end
