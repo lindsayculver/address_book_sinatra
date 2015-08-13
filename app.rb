7bd1d6ec1c82aab6e227c992553b81698e1b4dff
tony navarro & lindsay culver <student@epicodus.com>
2015-08-13 13:47:40 -0700

    creates contact_form.rb and contacts.erb within the views folder and changes app.rb to handle the  new webpages and links. Buttons are still not functional

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require('sinatra')
 require('sinatra/reloader')
-also_reload('.lib/**/*.rb')
+also_reload('lib/**/*.rb')
 require('./lib/contact')
 require('./lib/name')
 require('./lib/address')
@@ -8,4 +8,17 @@ require('./lib/phone')
 
 get('/') do
   erb(:index)
+end
+
+get('/contacts/new') do
+  erb(:contact_form)
+end
+
+get('/contacts') do
+  @contacts = Contact.all()
+  erb(:contacts)
+end
+
+get('/contacts/:id') do
+  @contacts = Contact.new(params.fetch('id').to_i())
 end
